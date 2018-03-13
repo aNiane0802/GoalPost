423448fecb869268a81fe1603af87134785acac4
Aboubakrine Niane <aboubakrineniane@Aboubakrines-MacBook-Pro.local>
2018-03-13 21:46:18 +0100

    First Commit

---
+++ GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Aboubakrine Niane on 13/03/2018.
+//  Copyright © 2018 Aboubakrine Niane. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
